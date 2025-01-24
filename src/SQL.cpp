0333d7da6b67116a2f8762d4052678ee6a3315e6
Alexey Puchkov <alexey.pkv@gmail.com>
2025-01-24 21:26:07 +0200

    Linking with sqlighter

--- src/SQL.cpp
+++ src/SQL.cpp
@@ -3,11 +3,24 @@
 
 #include "godotlighter_version.h"
 
+#include <sqlite3.h>
+#include <sqlighter.h>
+
 
 using namespace godotlighter;
 
 
+const char* SQL::sqlighter_version()
+{
+	return SQLIGHTER_VERSION;
+}
+
 const char* SQL::library_version()
 {
 	return GODOTLIGHTER_VERSION;
+}
+
+const char* SQL::sqlite_version()
+{
+	return sqlite3_libversion();
 }
--- src/SQL.h
+++ src/SQL.h
@@ -2,11 +2,19 @@
 #define GODOTLIGHTER_H
 
 
+namespace sqlighter
+{
+	class SQLighter;
+}
+
+
 namespace godotlighter
 {
 	class SQL
 	{
 	public:
+		static const char* sqlite_version();
+		static const char* sqlighter_version();
 		static const char* library_version();
 	};
 }
