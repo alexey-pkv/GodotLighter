a094c5ff471107c3ce6f8c7367ec677d1f11caca
Alexey Puchkov <alexey.pkv@gmail.com>
2025-02-16 08:40:32 +0200

    Working on Win build

---
+++ cmake/demos.cmake
@@ -0,0 +1,38 @@
+function(define_demo_target)
+	if (UNIX)
+		set(LIB_NAME libgodotlighter)
+		set(LIB_EXT so)
+	elseif(WIN32)
+		set(LIB_NAME godotlighter)
+		set(LIB_EXT dll)
+	endif ()
+	
+	file(GLOB DEMO_DIRS
+		"${CMAKE_SOURCE_DIR}/demos/*/*")
+	
+	set(FROM_FILE_EXT "${GODOTLIGHTER_OUT}/godotlighter.gdextension")
+	
+	
+	foreach (DEMO_DIR ${DEMO_DIRS})
+		MESSAGE(STATUS "Demo folder: ${DEMO_DIR}")
+		
+		
+		set(FROM_FILE_LIB "${GODOTLIGHTER_OUT}/${LIB_NAME}.${GODOTLIGHTER_PLATFORM}.template_debug.x86_64.${LIB_EXT}")
+		set(TO_FOLDER "${DEMO_DIR}/bin")
+		
+		
+		add_custom_command(
+			TARGET godotlighter POST_BUILD
+			COMMAND ${CMAKE_COMMAND} -E copy "${FROM_FILE_LIB}" "${TO_FOLDER}"
+			
+			COMMENT "Copying the library and .gdextension file to each demo/../bin directory"
+		)
+		
+#		add_custom_command(
+#			TARGET godotlighter POST_BUILD
+#			COMMAND ${CMAKE_COMMAND} -E copy "${FROM_FILE_EXT}" "${TO_FOLDER}"
+#
+#			COMMENT "Copying the library and .gdextension file to each demo/../bin directory"
+#		)
+	endforeach ()
+endfunction()
