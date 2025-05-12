9103c865c96bafe969ad0b2d459320e8f75a5699
Alexey Puchkov <alexey.pkv@gmail.com>
2025-05-12 10:37:40 +0300

    Minor standard fixes

--- cmake/sqlighter.cmake
+++ cmake/sqlighter.cmake
@@ -1,15 +1,15 @@
 include(FetchContent)
 
 
-set(GODOTLIGHTER_SQLIGHTER_VERISON "v0.3.0")
+set(GODOTLIGHTER_SQLIGHTER_VERSION "v0.3.0")
 
 
-message(STATUS "Fetching SQLighter ${GODOTLIGHTER_SQLIGHTER_VERISON}")
+message(STATUS "Fetching SQLighter ${GODOTLIGHTER_SQLIGHTER_VERSION}")
 
 FetchContent_Declare(
 	sqlighter
 	GIT_REPOSITORY	https://github.com/alexey-pkv/sqlighter.git
-	GIT_TAG			${GODOTLIGHTER_SQLIGHTER_VERISON}
+	GIT_TAG			${GODOTLIGHTER_SQLIGHTER_VERSION}
 )
 
 set(SQLIGHTER_BUILD_STATIC_LIBS ON)
